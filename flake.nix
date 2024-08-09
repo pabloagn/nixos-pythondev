6fc2db032804e69cde890c97359c3b1e92b38e25
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2024-08-08 20:16:13 -0600

    Created repo

---
+++ flake.nix
@@ -0,0 +1,31 @@
+# flake.nix
+# https://www.youtube.com/watch?v=yQwW8dkuHqw
+{
+  description = "test";
+
+  inputs =
+    {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default =
+        pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              python39
+            ];
+
+            shellHook = ''
+            zsh
+            '';
+
+          };
+    };
+}
+
